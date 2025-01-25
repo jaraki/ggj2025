669a222481b20d567aaaee49edcf252e96c0f7ae
jfc3 <buckslice@gmail.com>
2025-01-25 15:59:19 -0800

    add bubble pop particles. updated rat prefab

--- Assets/Game/Bubble.cs
+++ Assets/Game/Bubble.cs
@@ -1,28 +1,39 @@
 using UnityEngine;
 
-public class Bubble : MonoBehaviour
-{
+public class Bubble : MonoBehaviour {
 
     Rigidbody rb;
-    Light l;
-    const float maxLifeTime = 8.0f;
-    float lifeTime = maxLifeTime;
+    Light light;
+    public float maxLifeTime = 5.0f;
+    float lifeTime;
+    Renderer rend;
+    ParticleSystem ps;
+    bool die = false;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
+    void Start() {
+        lifeTime = maxLifeTime;
         rb = GetComponent<Rigidbody>();
-        l = rb.GetComponent<Light>();
+        light = GetComponentInChildren<Light>();
+        rend = GetComponentInChildren<Renderer>();
+        ps = GetComponentInChildren<ParticleSystem>();
     }
 
     // Update is called once per frame
-    void FixedUpdate()
-    {
+    void FixedUpdate() {
+        if (die) {
+            return;
+        }
         rb.AddForce(Vector3.down * 0.5f, ForceMode.Acceleration);
         lifeTime -= Time.deltaTime;
-        l.intensity = Mathf.Lerp(0.0f, 1.0f, lifeTime/maxLifeTime);
-        if(lifeTime < 0.0f) {
-            Destroy(gameObject);
+        if (light) {
+            light.intensity = Mathf.Lerp(0.0f, 1.0f, lifeTime / maxLifeTime);
+        }
+        if (lifeTime < 0.0f) {
+            Destroy(gameObject, 1.0f);
+            ps.Play();
+            rend.enabled = false;
+            die = true;
         }
     }
 }
