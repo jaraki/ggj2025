a2bedba77a3086f1be341e1a0cfe8098b6034c0f
jfc3 <buckslice@gmail.com>
2025-01-26 15:39:49 -0800

    add boss to boss room, final touches

--- Assets/Scripts/Boss.cs
+++ Assets/Scripts/Boss.cs
@@ -10,7 +10,6 @@ public class Boss : MonoBehaviour {
     public LayerMask targetingMask;
     float attackTimer = 0.0f;
     Animator anim;
-    Rigidbody rigid;
     float damageTimer = 0.0f;
     float soundTimer = 0.0f;
     public GameObject bubPrefab;
@@ -18,8 +17,6 @@ public class Boss : MonoBehaviour {
     public List<AudioClip> bossSounds = new List<AudioClip>();
 
     void Start() {
-        soundTimer = Random.Range(10.0f, 20.0f);
-        rigid = GetComponent<Rigidbody>();
         anim = GetComponentInChildren<Animator>();
         anim.speed = Random.Range(0.9f, 1.1f);
         player = FindFirstObjectByType<PlayerController>();
@@ -36,25 +33,23 @@ public class Boss : MonoBehaviour {
 
     }
 
-    //private void OnCollisionEnter(Collision collision) {
-    //    if (collision.collider.CompareTag("Bubble")) {
-    //        var bub = collision.collider.GetComponent<Bubble>();
-    //        bub.Die();
-
-    //        int lastIndex = healthObjects.Count - 1;
-    //        var last = healthObjects[lastIndex];
-    //        last.SetActive(false);
-    //        healthObjects.RemoveAt(lastIndex);
-    //        Game.Instance.BubblitisCount--;
-    //        anim.SetTrigger("damaged");
-    //        AudioManager.Instance.PlaySound(transform.position + Vector3.up, AudioManager.Instance.zombieDamage, 1.0f, Random.Range(0.5f, 0.6f));
-    //        if (lastIndex == 0) {
-    //            Destroy(gameObject);
-    //        }
-    //    }
-    //}
-
     void Update() {
+        damageTimer -= Time.deltaTime;
+        for (int i = 0; i < healthObjects.Count; i++) {
+            if (healthObjects[i] == null) {
+                healthObjects.RemoveAt(i);
+                if (damageTimer < 0.0f) {
+                    AudioManager.Instance.PlaySound(transform.position + Vector3.up, AudioManager.Instance.zombieDamage, 1.0f, Random.Range(1.5f, 1.6f));
+                    damageTimer = 2.0f;
+                }
+                break;
+            }
+        }
+
+        if (healthObjects.Count == 0) {
+            Destroy(gameObject);
+        }
+
         transform.Rotate(Vector3.up * Time.deltaTime * 20.0f); // just spin menacingly
         soundTimer -= Time.deltaTime;
         //damageTimer -= Time.deltaTime;
@@ -66,7 +61,7 @@ public class Boss : MonoBehaviour {
         //}
 
         if (soundTimer < 0.0f) {
-            soundTimer = Random.Range(5.0f, 8.0f);
+            soundTimer = Random.Range(2.0f, 5.0f);
             AudioManager.Instance.PlayRandomSoundFromList(transform.position, bossSounds, 0.5f, Random.Range(0.8f, 1.2f));
         }
 
